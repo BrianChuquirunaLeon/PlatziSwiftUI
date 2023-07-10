db3ab08e4233470237e8446d95e8101f61f6510e
brianchuquiruna <bryan_virgo_6@hotmail.com>
2023-07-10 02:07:52 -0500

    Initial Commit

---
+++ Platzi/PlatziApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziApp.swift
+//  Platzi
+//
+//  Created by brianchuquiruna on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PlatziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
